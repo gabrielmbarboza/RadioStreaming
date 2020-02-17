ac10e5ea79adf3f423642a5362f60db2b6ecc699
Gabriel Barboza <gabrielmbarboza@gmail.com>
2020-02-16 22:51:08 -0300

    Cria projeto usando expo

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
